0e61e41069170e20c52b41d265d8e4f7ae574dd3
exitfiat <felb@tuta.io>
2022-08-22 15:49:07 +0200

    clean up checkinvoice

--- payment.py
+++ payment.py
@@ -12,13 +12,12 @@ headers = {
 data = '{"out": false, "amount":1000, "memo":"dalle2bot", "unit":"sat"}'
 
 def getinvoice():
-  response = requests.post('https://legend.lnbits.com/api/v1/payments', headers=headers,   data=data)
+  response = requests.post('https://legend.lnbits.com/api/v1/payments', headers=headers, data=data)
   return response.json()
 
 def checkinvoice(payment_hash):
-  response = requests.get('https://legend.lnbits.com/api/v1/payments/'+payment_hash, headers=headers, data=data)
-  result = response.json()
-  return result['paid']
+  response = requests.get('https://legend.lnbits.com/api/v1/payments/'+payment_hash, headers=headers)
+  return response.json()['paid']
 
 #def refund():
 #   sth sth lnurlw
