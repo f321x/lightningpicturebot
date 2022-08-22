edce03d1c2b787c486f3b43238fdca6194e0bb8f
exitfiat <felb@tuta.io>
2022-08-22 11:29:55 +0200

    added empty openai token message to violation message to create less confusion in case the tokens run out

--- messages.py
+++ messages.py
@@ -12,7 +12,7 @@ help = "Type in a command for DALLE2 like the examples you can find on the OpenA
        "on Telegram @f321x or read the source code of this Bot on github to understand how it works. If you encounter " \
        "any problems or didn't get a result press /problem . Press /source to see the source code of the bot "
 
-terms = "Common woke stuff, so just don't try anything controversial"
+terms = "https://labs.openai.com/policies/terms"
 
 problem = "If you encounter any problem please contact @f321x on TG and i will refund or help you. If you paid but " \
           "didn't get a result please make a screenshot and send it to me with a refund invoice. The reason for the " \
@@ -20,6 +20,8 @@ problem = "If you encounter any problem please contact @f321x on TG and i will r
           "command. "
 
 violation = "Your message probably violated the OpenAI /terms so DALLE didn't return pictures. I paid for this OpenAI " \
-            "cycle anyways but can understand if you want your sats back so don't hesitate to contact me @f321x "
+            "cycle anyways but can understand if you want your sats back so don't hesitate to contact me @f321x " \
+            "If this message comes up every time, even though your commands aren't against the /terms in any my " \
+            "OpenAI account probably ran out of Tokens please contact me so i can refill them "
 
 source = "https://github.com/exitfiat/dalle2telegrambot"
