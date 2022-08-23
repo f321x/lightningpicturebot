a0a303f1062d5c708bbd5d85cad98ed70cfafb43
exitfiat <felb@tuta.io>
2022-08-23 21:16:16 +0200

    formatted invoice as code to make it click to copy

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
                                  photo=open(str(update.effective_chat.id) + ".png", 'rb'))
     os.remove(str(update.effective_chat.id) + ".png")
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text="lightning:" + user_state[update.effective_chat.id][1]['payment_request'])
+                                   text="`lightning:" + user_state[update.effective_chat.id][1]['payment_request']+"`")
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Press /generate once you paid the invoice")
 
 
