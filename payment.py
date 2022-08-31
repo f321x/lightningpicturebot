c14d4327299964e7b65edd95a57632acd286ae7c
exitfiat <felb@tuta.io>
2022-08-31 16:47:00 +0200

    back to lnbits legend

--- payment.py
+++ payment.py
@@ -13,12 +13,12 @@ data = '{"out": false, "amount":1000, "memo":"lightningpicturebot", "unit":"sat"
 
 
 def getinvoice():
-    response = requests.post('https://192.168.0.72:5001/api/v1/payments', headers=headers, data=data)
+    response = requests.post('https://legend.lnbits.com/api/v1/payments', headers=headers, data=data)
     return response.json()
 
 
 def checkinvoice(payment_hash):
-    response = requests.get('https://192.168.0.72:5001/api/v1/payments' + payment_hash, headers=headers)
+    response = requests.get('https://legend.lnbits.com/api/v1/payments' + payment_hash, headers=headers)
     return response.json()['paid']
 
 # def refund():
