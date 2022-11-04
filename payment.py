5604adc471af218257b3cc0971caf2585ef81567
exitfiat <felb@tuta.io>
2022-11-04 20:41:04 +0100

    more reliable payments

--- payment.py
+++ payment.py
@@ -18,20 +18,19 @@ data = '{"out": false, "amount":1000, "memo":"lightningpicturebot", "unit":"sat"
 
 
 def getinvoice():
-    response = requests.post('https://192.168.0.72:5001/api/v1/payments', headers=headers, data=data, verify=False)
+    response = requests.post('https://legend.lnbits.com/api/v1/payments', headers=headers, data=data)
     return response.json()
 
 
 def checkinvoice(payment_hash):
-    response = requests.get('https://192.168.0.72:5001/api/v1/payments/' + payment_hash, headers=headers, verify=False)
+    response = requests.get('https://legend.lnbits.com/api/v1/payments/' + payment_hash, headers=headers)
     return response.json()['paid']
 
 
 def refund(invoice):
     if invoice[4:7] == '10u' or invoice[14:17] == '10u':
         data = '{"out": true, "bolt11": "' + invoice + '"}'
-        response = requests.post('https://192.168.0.72:5001/api/v1/payments', headers=headers_refund, data=data,
-                                 verify=False)
+        response = requests.post('https://legend.lnbits.com/api/v1/payments', headers=headers_refund, data=data)
         if response.status_code == 201:
             return "success"
         else:
