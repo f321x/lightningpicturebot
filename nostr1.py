90c59c0703a6d8da4ef259acf77e46a5e11c5370
f321x <felb@tuta.io>
2022-12-17 15:24:23 +0100

    added another relay to nostr

--- nostr1.py
+++ nostr1.py
@@ -1,4 +1,4 @@
-#needs rclone set up with dropbox, running
+# needs rclone set up with dropbox, running
 import os
 import ssl
 import time
@@ -35,10 +35,11 @@ filters_gc = Filter(limit=0, kinds=[42], tags={"#e": [os.environ['nostr_chat_id'
 filters = Filters([filters_pm, filters_gc])
 relay_manager = RelayManager()
 
+
 def connect():
     count = 0
     try:
-        relay_manager.add_relay("wss://nostr-pub.wellorder.net")
+        relay_manager.add_relay("wss://relay.grunch.dev")
         relay_manager.add_relay("wss://relay.damus.io")
         relay_manager.add_subscription(subscription_id, filters)
         relay_manager.open_connections({"cert_reqs": ssl.CERT_NONE})
@@ -48,6 +49,8 @@ def connect():
         count += 1
         print("connecting failed " + str(count) + " times!")
         connect()
+
+
 def nostr_dalle():
     request = [ClientMessageType.REQUEST, subscription_id]
     request.extend(filters.to_json_array())
@@ -73,7 +76,9 @@ def nostr_dalle():
                     message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
                     relay_manager.publish_message(message_2)
                     time.sleep(2)  # allow the messages to send
-                    event = Event(public_key, "Send /gd (DALLE2) or /gsd (Stable Diffusion) or /gmj (Midjourney like, experimental, 2 pics) once you paid the invoice to start generating", kind=42,
+                    event = Event(public_key,
+                                  "Send /gd (DALLE2) or /gsd (Stable Diffusion) or /gmj (Midjourney like, experimental, 2 pics) once you paid the invoice to start generating",
+                                  kind=42,
                                   tags=[["e", os.environ['nostr_chat_id']]], created_at=int(time.time()))
                     event.sign(private_key)
                     message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
@@ -100,7 +105,9 @@ def nostr_dalle():
                         dalle_generate(current_prompt, 42, None)
                         current_prompt = ""
                     elif payment.checkinvoice(user_state_nostr[current_prompt]['payment_hash']) != True:
-                        event = Event(public_key, "You havent paid yet, send /gd again once you paid or give a new prompt with /p", kind=42,
+                        event = Event(public_key,
+                                      "You havent paid yet, send /gd again once you paid or give a new prompt with /p",
+                                      kind=42,
                                       tags=[["e", os.environ['nostr_chat_id']]], created_at=int(time.time()))
                         event.sign(private_key)
                         message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
@@ -157,15 +164,19 @@ def nostr_dalle():
                     connect()
                     current_prompt = content[3:]
                     user_state_nostr[current_prompt] = payment.getinvoice()
-                    event = Event(public_key, encrypt_message(str("https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=" +
-                                                                  user_state_nostr[current_prompt]['payment_request'] + "&format=.png") + " " +
-                                                              str(user_state_nostr[current_prompt]['payment_request']), ss), kind=4,
+                    event = Event(public_key,
+                                  encrypt_message(str("https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=" +
+                                                      user_state_nostr[current_prompt][
+                                                          'payment_request'] + "&format=.png") + " " +
+                                                  str(user_state_nostr[current_prompt]['payment_request']), ss), kind=4,
                                   tags=[["p", user_pk]], created_at=int(time.time()))
                     event.sign(private_key)
                     message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
                     relay_manager.publish_message(message_2)
                     time.sleep(2)  # allow the messages to send
-                    event = Event(public_key, encrypt_message("Send /gd (DALLE2) or /gsd (Stable Diffusion) or /gmj (Midjourney like, experimental, 2 pics) once you paid the invoice to start generating", ss), kind=4,
+                    event = Event(public_key, encrypt_message(
+                        "Send /gd (DALLE2) or /gsd (Stable Diffusion) or /gmj (Midjourney like, experimental, 2 pics) once you paid the invoice to start generating",
+                        ss), kind=4,
                                   tags=[["p", user_pk]], created_at=int(time.time()))
                     event.sign(private_key)
                     message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
@@ -192,7 +203,9 @@ def nostr_dalle():
                         dalle_generate(current_prompt, 4, user_pk)
                         current_prompt = ""
                     elif payment.checkinvoice(user_state_nostr[current_prompt]['payment_hash']) != True:
-                        event = Event(public_key, encrypt_message("You havent paid yet, send /gd again once you paid or give a new prompt with /p", ss), kind=4,
+                        event = Event(public_key, encrypt_message(
+                            "You havent paid yet, send /gd again once you paid or give a new prompt with /p", ss),
+                                      kind=4,
                                       tags=[["p", user_pk]], created_at=int(time.time()))
                         event.sign(private_key)
                         message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
@@ -212,7 +225,9 @@ def nostr_dalle():
                         current_prompt = ""
                     elif payment.checkinvoice(user_state_nostr[current_prompt]['payment_hash']) != True:
                         event = Event(public_key,
-                                      encrypt_message("You havent paid yet, send /gsd again once you paid or give a new prompt with /p", ss),
+                                      encrypt_message(
+                                          "You havent paid yet, send /gsd again once you paid or give a new prompt with /p",
+                                          ss),
                                       kind=4,
                                       tags=[["p", user_pk]], created_at=int(time.time()))
                         event.sign(private_key)
@@ -233,7 +248,9 @@ def nostr_dalle():
                         current_prompt = ""
                     elif payment.checkinvoice(user_state_nostr[current_prompt]['payment_hash']) != True:
                         event = Event(public_key,
-                                      encrypt_message("You havent paid yet, send /gmj again once you paid or give a new prompt with /p", ss),
+                                      encrypt_message(
+                                          "You havent paid yet, send /gmj again once you paid or give a new prompt with /p",
+                                          ss),
                                       kind=4,
                                       tags=[["p", user_pk]], created_at=int(time.time()))
                         event.sign(private_key)
@@ -294,7 +311,8 @@ def nostr_midjourney(prompt, message_type, user_pubk):
                 m = image.replace(os.getcwd() + "/", "")
                 link = list(rc.link('dropbox:lpb/' + m))
                 link[-2] = '1'
-                event = Event(public_key, encrypt_message(''.join(link) + " " + prompt + ", Midjourney model", ss), kind=4,
+                event = Event(public_key, encrypt_message(''.join(link) + " " + prompt + ", Midjourney model", ss),
+                              kind=4,
                               tags=[["p", user_pubk]], created_at=int(time.time()))
                 event.sign(private_key)
                 message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
@@ -303,13 +321,16 @@ def nostr_midjourney(prompt, message_type, user_pubk):
         rc.execute('delete --min-age 360d dropbox:lpb')
         time.sleep(1)  # allow the messages to send
 
+
 def dalle_generate(prompt, type, user_pk):
     id = str(int(time.time()))
     generations = dalle2.generate_and_download(str(prompt), id)
     if type == 42:
         if generations == "violation":
             logging.info("nostr dalle violation: " + prompt)
-            event = Event(public_key, "Your prompt violated the OpenAI terms so it got blocked by them. Try to avoid controversial stuff or use Stable Diffusion with /gsd", kind=42,
+            event = Event(public_key,
+                          "Your prompt violated the OpenAI terms so it got blocked by them. Try to avoid controversial stuff or use Stable Diffusion with /gsd",
+                          kind=42,
                           tags=[["e", os.environ['nostr_chat_id']]], created_at=int(time.time()))
             event.sign(private_key)
             message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
@@ -318,7 +339,8 @@ def dalle_generate(prompt, type, user_pk):
         elif generations == "failure":
             logging.error("dalle error nostr: " + prompt)
             event = Event(public_key, "Whoops, this failed. Sometimes the Dalle API is unreliable, send an invoice and "
-                                      "@f321x will try to refund you. You can try again later.", kind=42, tags=[["e", os.environ['nostr_chat_id']]], created_at=int(time.time()))
+                                      "@f321x will try to refund you. You can try again later.", kind=42,
+                          tags=[["e", os.environ['nostr_chat_id']]], created_at=int(time.time()))
             event.sign(private_key)
             message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
             relay_manager.publish_message(message_2)
@@ -344,7 +366,9 @@ def dalle_generate(prompt, type, user_pk):
         if generations == "violation":
             logging.info("nostr dalle violation: " + prompt)
             event = Event(public_key,
-                          encrypt_message("Your prompt violated the OpenAI terms so it got blocked by them. Try to avoid controversial stuff or use Stable Diffusion with /gsd", ss),
+                          encrypt_message(
+                              "Your prompt violated the OpenAI terms so it got blocked by them. Try to avoid controversial stuff or use Stable Diffusion with /gsd",
+                              ss),
                           kind=4,
                           tags=[["p", user_pk]], created_at=int(time.time()))
             event.sign(private_key)
@@ -353,8 +377,9 @@ def dalle_generate(prompt, type, user_pk):
             time.sleep(1.25)  # allow the messages to send
         elif generations == "failure":
             logging.error("dalle error nostr: " + prompt)
-            event = Event(public_key, encrypt_message("Whoops, this failed. Sometimes the Dalle API is unreliable, send an invoice and "
-                                      "@f321x will try to refund you. You can try again later.", ss), kind=4,
+            event = Event(public_key, encrypt_message(
+                "Whoops, this failed. Sometimes the Dalle API is unreliable, send an invoice and "
+                "@f321x will try to refund you. You can try again later.", ss), kind=4,
                           tags=[["p", user_pk]], created_at=int(time.time()))
             event.sign(private_key)
             message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
@@ -376,6 +401,7 @@ def dalle_generate(prompt, type, user_pk):
                 time.sleep(1.25)  # allow the messages to send
             rc.execute('delete --min-age 360d dropbox:lpb')
             time.sleep(1)  # allow the messages to send
+
 
 def sd_generate(prompt, type, user_pk):
     id = str(int(time.time()))
@@ -435,14 +461,17 @@ def sd_generate(prompt, type, user_pk):
         ss = compute_shared_secret(private_key, user_pk)
         for generating in range(2):
             if stablediffusion.find_seed(prompt) == "seed_too_long":
-                event = Event(public_key, encrypt_message("Seed too long, use max. 9 digits, generating without seed now.", ss), kind=4,
+                event = Event(public_key,
+                              encrypt_message("Seed too long, use max. 9 digits, generating without seed now.", ss),
+                              kind=4,
                               tags=[["p", user_pk]], created_at=int(time.time()))
                 event.sign(private_key)
                 message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
                 relay_manager.publish_message(message_2)
                 time.sleep(1)
             elif stablediffusion.find_seed(prompt) == "seed_no_int":
-                event = Event(public_key, encrypt_message("Seed not an integer (number), generating without seed now.", ss), kind=4,
+                event = Event(public_key,
+                              encrypt_message("Seed not an integer (number), generating without seed now.", ss), kind=4,
                               tags=[["p", public_key]], created_at=int(time.time()))
                 event.sign(private_key)
                 message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
@@ -455,7 +484,8 @@ def sd_generate(prompt, type, user_pk):
                     os.remove('sd_picture_gd_' + str(guidance) + "_" + id + '.png')
                     link = list(rc.link('dropbox:lpb/' + 'sd_picture_gd_' + str(guidance) + "_" + id + '.png'))
                     link[-2] = '1'
-                    event = Event(public_key, encrypt_message(''.join(link) + " " + prompt + ", Stable Diffusion", ss), kind=4,
+                    event = Event(public_key, encrypt_message(''.join(link) + " " + prompt + ", Stable Diffusion", ss),
+                                  kind=4,
                                   tags=[["p", user_pk]], created_at=int(time.time()))
                     event.sign(private_key)
                     message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
@@ -467,8 +497,9 @@ def sd_generate(prompt, type, user_pk):
             except:
                 if generating == 0:
                     logging.error("nostr sd error: " + prompt)
-                    event = Event(public_key, encrypt_message("Failed, trying again. If it doesn't give you pictures in a minute click "
-                                              "/problem", ss), kind=4, tags=[["p", user_pk]],
+                    event = Event(public_key, encrypt_message(
+                        "Failed, trying again. If it doesn't give you pictures in a minute click "
+                        "/problem", ss), kind=4, tags=[["p", user_pk]],
                                   created_at=int(time.time()))
                     event.sign(private_key)
                     message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
@@ -476,8 +507,9 @@ def sd_generate(prompt, type, user_pk):
                     time.sleep(15)
                 elif generating == 1:
                     logging.error("nostr sd error: " + prompt)
-                    event = Event(public_key, encrypt_message("Failed, trying again. If it doesn't give you pictures in a minute click "
-                                              "/problem", ss), kind=4, tags=[["p", user_pk]],
+                    event = Event(public_key, encrypt_message(
+                        "Failed, trying again. If it doesn't give you pictures in a minute click "
+                        "/problem", ss), kind=4, tags=[["p", user_pk]],
                                   created_at=int(time.time()))
                     event.sign(private_key)
                     message_2 = json.dumps([ClientMessageType.EVENT, event.to_json_object()])
@@ -487,4 +519,3 @@ def sd_generate(prompt, type, user_pk):
 
 connect()
 nostr_dalle()
-
